3d0ec3fb26e21e5751f5eb9224d2d23ff8b0020a
halilmek <62161903+halilmek@users.noreply.github.com>
2020-03-14 22:38:16 +0200

    commit 8

--- src/gitPackage/NextClass.java
+++ src/gitPackage/NextClass.java
@@ -5,6 +5,7 @@ public class NextClass {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello guys!");
+		System.out.println("Hello dear friends!");
 	}
 
 }
