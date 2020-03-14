33f0125f21e7973f8bed723d865659f05d5b23eb
halil ozturk <ztrkhll58@gmail.com>
2020-03-14 22:07:30 +0200

    commit six added

--- src/gitPackage/GitDay2.java
+++ src/gitPackage/GitDay2.java
@@ -3,9 +3,10 @@ package gitPackage;
 public class GitDay2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println("Hello guys!");
-		System.out.println("hello git users");
+System.out.println("Hello World!");
+System.out.println("Hello guys!");
+System.out.println("hello git users");
+System.out.println("check if your local is ahead of origin master");
 
 	}
 
