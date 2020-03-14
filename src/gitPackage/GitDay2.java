dfc5ab4258bff8ad0bc4e329d9618a131be221f7
halil ozturk <ztrkhll58@gmail.com>
2020-03-14 22:17:52 +0200

    commit seven

--- src/gitPackage/GitDay2.java
+++ src/gitPackage/GitDay2.java
@@ -7,6 +7,7 @@ System.out.println("Hello World!");
 System.out.println("Hello guys!");
 System.out.println("hello git users");
 System.out.println("check if your local is ahead of origin master");
+System.out.println("commit 7");
 
 	}
 
