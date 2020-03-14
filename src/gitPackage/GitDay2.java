9923debe4de9a133d70a47ea457b5ebeaca35d18
halil ozturk <ztrkhll58@gmail.com>
2020-03-14 21:57:54 +0200

    hellooo

--- src/gitPackage/GitDay2.java
+++ src/gitPackage/GitDay2.java
@@ -5,6 +5,7 @@ public class GitDay2 {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello guys!");
+		System.out.println("hello git users");
 
 	}
 
